626e2cef51bc91ddb539990910588a076db682ab
unknown <andrew.potapenko47@gmail.com>
2020-05-28 00:49:27 +0300

    first commit

---
+++ task-2.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(obj) {
+      this.name = obj.name;
+      this.age = obj.age;
+      this.followers = obj.followers;
+    }
+  
+    get info() {
+      return `User ${this.name} is ${this.age} years old and has ${this.followers} followers`;
+    }
+  }
+  
+  const mango = new User({
+    name: 'Mango',
+    age: 2,
+    followers: 20,
+  });
+  
+  console.log(mango.info);
+  
+  const poly = new User({
+    name: 'Poly',
+    age: 3,
+    followers: 17,
+  });
+  console.log(poly.info);
